4c12baeba42d92d5929fd0278c2cd5ce36149f66
Justin Guerré <guerrejustin@gmail.com>
2022-01-16 19:29:27 +0100

    add padding to tailwindcss container

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    container: {
+      padding: "1rem",
+    },
     extend: {},
   },
-  darkMode: 'class',
+  darkMode: "class",
   plugins: [],
 };
