309c8c3ab856451a1a82ac2ad1d8ff4d6705f9d3
Justin Guerré <guerrejustin@gmail.com>
2022-01-16 19:30:15 +0100

    style post component

--- components/Post.tsx
+++ components/Post.tsx
@@ -6,12 +6,32 @@ type PostProps = { post: PostWithUser };
 
 const PostComponent = ({ post: { content, id, User } }: PostProps) => {
   return (
-    <div
-      key={id}
-      className="my-3 p-5 bg-slate-200 dark:bg-slate-800 rounded-md"
-    >
-      <p>{content}</p>
-      <Link href={`/${id}`}>
+    <Link href={`/${id}`} passHref>
+      <a
+        key={id}
+        className="flex my-3 px-3 py-4 bg-zinc-200 dark:bg-zinc-800 dark:hover:bg-zinc-700 hover:bg-zinc-300 rounded-md space-x-2"
+      >
+        <div>
+          {User.image && (
+            <div className="relative w-10 aspect-square">
+              <Image
+                layout="fill"
+                className="rounded-full"
+                alt="Author profile image"
+                src={User.image}
+              />
+            </div>
+          )}
+        </div>
+        <div className="flex-1">
+          <Link href="/">
+            <a className="no-underline hover:underline transition-all">
+              {User.name && User.name}
+            </a>
+          </Link>
+          <div className="text-sm opacity-70">{content}</div>
+        </div>
+        {/* <Link href={`/${id}`}>
         <a
           className="transition underline
                    decoration-orange-500 hover:text-orange-500
@@ -19,18 +39,9 @@ const PostComponent = ({ post: { content, id, User } }: PostProps) => {
         >
           Open the post
         </a>
-      </Link>
-      {User.image && (
-        <div className="relative w-10 aspect-square">
-          <Image
-            layout="fill"
-            className="rounded-full"
-            alt="Author profile image"
-            src={User.image}
-          />
-        </div>
-      )}
-    </div>
+      </Link> */}
+      </a>
+    </Link>
   );
 };
 
