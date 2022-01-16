a64df52a29147bba9a74b91d08e2dff5195cbdc7
Justin Guerré <guerrejustin@gmail.com>
2022-01-16 13:46:03 +0100

    use user.id on post create

--- pages/api/post/create.ts
+++ pages/api/post/create.ts
@@ -1,15 +1,22 @@
 import { Post } from "@prisma/client";
 import type { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/react";
 import prisma from "../../../lib/prisma";
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Post | string>
+  res: NextApiResponse<Post | { message: string }>
 ) {
   if (req.method !== "POST") {
-    return res.status(405).json("Method not allowed");
+    return res.status(405).json({ message: "Method not allowed" });
+  }
+  const session = await getSession({ req });
+  if (!session) {
+    return res.status(401).json({ message: "Unauthorized" });
   }
   const newPost = JSON.parse(req.body);
-  const savedPost = await prisma.post.create({ data: newPost });
+  const savedPost = await prisma.post.create({
+    data: { ...newPost, authorId: session.user.id },
+  });
   res.status(200).json(savedPost);
 }
