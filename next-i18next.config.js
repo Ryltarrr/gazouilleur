dc97e8333914020ce9df511902e26f81232c6866
Justin Guerré <guerrejustin@gmail.com>
2022-03-08 16:47:41 +0100

    fix translations path

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,10 @@
+const path = require("path");
+
 module.exports = {
   i18n: {
     defaultLocale: "en",
     locales: ["en", "fr"],
     reloadOnPrerender: process.env.NODE_ENV === "development",
+    localePath: path.resolve("./public/locales"),
   },
 };
